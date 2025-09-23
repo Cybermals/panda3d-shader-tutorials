c8b150e8416ba3fe65492b5a1ba1d7683a99a730
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-09-23 01:46:18 -0400

    Adjust ambient lighting in PBR lesson 2.

--- pbr/02-shadeless_rendering/main.py
+++ pbr/02-shadeless_rendering/main.py
@@ -25,7 +25,7 @@ class ShaderDemo(ShowBase):
 
         # Setup lighting
         self.ambient_light = self.render.attach_new_node(AmbientLight("AmbientLight"))
-        self.ambient_light.node().set_color(Vec4(.04, .04, .04, 1))
+        self.ambient_light.node().set_color(Vec4(.2, .2, .2, 1))
         self.render.set_light(self.ambient_light)
 
         self.sun = self.render.attach_new_node(DirectionalLight("Sun"))
